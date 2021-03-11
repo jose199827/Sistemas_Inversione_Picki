870eb20f4cf14988d55ac5341a9fb6d00050c56c
LexGarMont97 <marioalexgarciamont@yahoo.com>
2021-03-11 00:48:17 -0600

    Avance_ALEX_7/03/2021

--- clientes.php
+++ clientes.php
@@ -4,7 +4,7 @@
 <head>
   <!-- Basic Page Info -->
   <meta charset="utf-8">
-  <title>DeskApp - Bootstrap Admin Dashboard HTML Template</title>
+  <title>Inversiones Picky</title>
 
   <!-- Site favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
@@ -20,6 +20,8 @@
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
   <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
+  <link rel="stylesheet" type="text/css" href="src/plugins/datatables/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="src/plugins/datatables/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
 
 
@@ -195,9 +197,10 @@
               <span class="micon dw dw-group"></span><span class="mtext">Clientes</span>
             </a>
             <ul class="submenu">
-              <li><a href="clientes.php">Clientes</a></li>
-              <li><a href="index2.html">Dashboard style 2</a></li>
-              <li><a href="index3.html">Dashboard style 3</a></li>
+              <li><a href="adjclientes.html">Agregar Clientes</a></li>
+              <li><a href="clientes.php">Tabla Control Clientes</a></li>
+              <!--<li><a href="index2.html">Dashboard style 2</a></li>
+              <li><a href="index3.html">Dashboard style 3</a></li> -->
             </ul>
           </li>
           <!-- Menu Proveedores -->
@@ -239,42 +242,123 @@
         </ul>
       </div>
     </div>
-  </div>
+  	</div>
 
-  <div class="mobile-menu-overlay"></div>
+	<div class="mobile-menu-overlay"></div>
 
-  <div class="main-container">
-    <div class="pd-ltr-20 xs-pd-20-10">
-      <div class="min-height-200px">
-        <div class="page-header">
-          <div class="row">
-            <div class="col-md-6 col-sm-12">
-              <div class="title">
-                <h4>Inicio</h4>
-              </div>
-              <nav aria-label="breadcrumb" role="navigation">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
-                </ol>
-              </nav>
+	<div class="main-container">
+		<div class="pd-ltr-20 xs-pd-20-10">
+			<div class="min-height-200px">
+				<div class="page-header">
+					<div class="row">
+						<div class="col-md-6 col-sm-12">
+							<div class="title">
+								<h4>Inversiones Picky</h4>
+							</div>
+							<nav aria-label="breadcrumb" role="navigation">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="clientes.php">Clientes</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Tabla Clientes</li>
+								</ol>
+							</nav>
+						</div>
+						<div class="col-md-6 col-sm-12 text-right">
+              			 <!--<button type="button" class="btn btn-primary btn-lg" href="bancos_tabla.html">Agregar Clientes</button> -->
+              			<!--<button type="button" class="btn btn-primary btn-lg" onclick="href='localhost/bancos_tabla.html'">Llévame a otro lado</button> -->
+                    	<a href="bancos_tabla.html"><input type="button" class="btn btn-primary btn-lg" value="Agregar Clientes"></a>
+  							</div>
+  						</div>
             </div>
 
-          </div>
-        </div>
-        <div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
-        </div>
-      </div>
-      <div class="footer-wrap pd-20 mb-20 card-box">
-        DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit
-          Hingarajiya</a>
-      </div>
-    </div>
-  </div>
-  <!-- js -->
-  <script src="vendors/scripts/core.js"></script>
-  <script src="vendors/scripts/script.min.js"></script>
-  <script src="vendors/scripts/process.js"></script>
-  <script src="vendors/scripts/layout-settings.js"></script>
-</body>
-
+					<!-- EMPIEZA TABLA -->
+					<div class="card-box mb-30">
+					<div class="pd-20">
+						<h4 class="text-blue h4">Tabla Control Clientes</h4>
+					</div>
+					<div class="pb-20">
+						<table class="table hover multiple-select-row data-table-export nowrap">
+							<thead>
+								<tr>
+									<th class="table-plus datatable-nosort">Núm</th>
+									<th>ID CLiente</th>
+									<th>Empresa</th>
+									<th>RTN Empresa</th>
+									<th>ID Tipo Cliente</th>
+									<th>Usr Registro</th>
+									<th class="datatable-nosort">Acción</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="table-plus">1</td>
+									</td>
+									<td>25</td>
+									<td>Global IT</td>
+									<td>0801199872515815 </td>
+									<td>12654</td>
+									<td>F365DF6</td>
+									<td>
+										<div class="dropdown">
+											<a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+												<i class="dw dw-more"></i>
+											</a>
+											<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+												<a class="dropdown-item" href="#"><i class="dw dw-eye"></i> Ver</a>
+												<a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Editar</a>
+												<a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Eliminar</a>
+											</div>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td class="table-plus">2</td>
+									<td>30</td>
+									<td>M&L TEC</td>
+									<td>071519762365847 </td>
+									<td>32980</td>
+									<td>ER63218</td>
+									<td>
+										<div class="dropdown">
+											<a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+												<i class="dw dw-more"></i>
+											</a>
+											<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+												<a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+												<a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+												<a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+											</div>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+            
+					</div>
+				</div>
+				<!-- Export Datatable End -->
+			</div>
+			<div class="footer-wrap pd-20 mb-20 card-box">
+				DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+	<script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+	<script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+	<!-- buttons for Export datatable -->
+	<script src="src/plugins/datatables/js/dataTables.buttons.min.js"></script>
+	<script src="src/plugins/datatables/js/buttons.bootstrap4.min.js"></script>
+	<script src="src/plugins/datatables/js/buttons.print.min.js"></script>
+	<script src="src/plugins/datatables/js/buttons.html5.min.js"></script>
+	<script src="src/plugins/datatables/js/buttons.flash.min.js"></script>
+	<script src="src/plugins/datatables/js/pdfmake.min.js"></script>
+	<script src="src/plugins/datatables/js/vfs_fonts.js"></script>
+	<!-- Datatable Setting js -->
+	<script src="vendors/scripts/datatable-setting.js"></script></body>
 </html>
--- prueba.php
+++ prueba.php
@@ -4,16 +4,13 @@
 <head>
   <!-- Basic Page Info -->
   <meta charset="utf-8">
-  <title>DeskApp - Bootstrap Admin Dashboard HTML Template</title>
-
+  <title>Sistema de Facturación Inversiones Picky</title>
   <!-- Site favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="vendors/images/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="vendors/images/favicon-16x16.png">
-
   <!-- Mobile Specific Metas -->
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
   <!-- Google Font -->
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
     rel="stylesheet">
@@ -21,19 +18,16 @@
   <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
   <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
   <link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
-
-
+  <link rel="stylesheet" type="text/css" href="src/plugins/jquery-steps/jquery.steps.css">
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
-
     gtag('config', 'UA-119386393-1');
   </script>
 </head>
-
 <body>
   <!-- Inicio del Precaargador -->
   <div class="pre-loader">
@@ -49,7 +43,6 @@
     </div>
   </div>
   <!-- Fin del Precaargador -->
-
   <!-- Inicio del Menu Principal -->
   <div class="header">
     <div class="header-left">
@@ -82,7 +75,6 @@
     </div>
   </div>
   <!-- Fin del Menu Principal -->
-
   <!-- Inicio del Menu lateral de diseño -->
   <div class="right-sidebar">
     <div class="sidebar-title">
@@ -101,13 +93,11 @@
           <a href="javascript:void(0);" class="btn btn-outline-primary header-white active">Blanco</a>
           <a href="javascript:void(0);" class="btn btn-outline-primary header-dark">Negro</a>
         </div>
-
         <h4 class="weight-600 font-18 pb-10">Fondo de la barra lateral</h4>
         <div class="sidebar-btn-group pb-30 mb-10">
           <a href="javascript:void(0);" class="btn btn-outline-primary sidebar-light ">Blanco</a>
           <a href="javascript:void(0);" class="btn btn-outline-primary sidebar-dark active">Negro</a>
         </div>
-
         <h4 class="weight-600 font-18 pb-10">Icono de menú desplegable</h4>
         <div class="sidebar-radio-group pb-10 mb-10">
           <div class="custom-control custom-radio custom-control-inline">
@@ -126,7 +116,6 @@
             <label class="custom-control-label" for="sidebaricon-3"><i class="fa fa-angle-double-right"></i></label>
           </div>
         </div>
-
         <h4 class="weight-600 font-18 pb-10">Icono de lista de menú</h4>
         <div class="sidebar-radio-group pb-30 mb-10">
           <div class="custom-control custom-radio custom-control-inline">
@@ -161,7 +150,6 @@
             <label class="custom-control-label" for="sidebariconlist-6"><i class="dw dw-next"></i></label>
           </div>
         </div>
-
         <div class="reset-options pt-30 text-center">
           <button class="btn btn-danger" id="reset-settings">Reiniciar ajustes</button>
         </div>
@@ -169,7 +157,6 @@
     </div>
   </div>
   <!-- Fin del Menu lateral de diseño -->
-
   <div class="left-side-bar">
     <div class="brand-logo">
       <a href="index.html">
@@ -195,7 +182,7 @@
               <span class="micon dw dw-group"></span><span class="mtext">Clientes</span>
             </a>
             <ul class="submenu">
-              <li><a href="clientes.php">Clientes</a></li>
+              <li><a href="index.html">Dashboard style 1</a></li>
               <li><a href="index2.html">Dashboard style 2</a></li>
               <li><a href="index3.html">Dashboard style 3</a></li>
             </ul>
@@ -235,33 +222,171 @@
               <li><a href="datatable.html">DataTables</a></li>
             </ul>
           </li>
-
         </ul>
       </div>
     </div>
   </div>
-
   <div class="mobile-menu-overlay"></div>
-
   <div class="main-container">
     <div class="pd-ltr-20 xs-pd-20-10">
       <div class="min-height-200px">
-        <div class="page-header">
-          <div class="row">
-            <div class="col-md-6 col-sm-12">
-              <div class="title">
-                <h4>Inicio</h4>
-              </div>
-              <nav aria-label="breadcrumb" role="navigation">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
-                </ol>
-              </nav>
-            </div>
-
+      
+        <div class="pd-20 card-box mb-30">
+          <div class="clearfix">
+            <h4 class="text-blue h4">Registrar Empleados</h4>
           </div>
-        </div>
-        <div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
+          <div class="wizard-content">
+            <form class="tab-wizard wizard-circle wizard">
+              <h5>Información Personal</h5>
+              <section>
+                <div class="row">
+                  <div class="col-md-6">     
+                    <div class="form-group">
+                      <label for="nombre">Nombre:</label>
+                      <input class="form-control form-control-lg" type="text" id="nombre">
+                  </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="apellido">Apellido:</label>
+                      <input type="text" class="form-control form-control-lg" id="apellido">
+                  </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="identidad">Num. Identidad:</label>
+                      <input type="text" class="form-control form-control-lg" id="identidad">
+                  </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="edad">Edad:</label>
+                      <input type="number" value="" class="form-control form-control-lg" id="edad">
+                  </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="nacimiento">Fecha de Nacimiento:</label>
+                      <input type="date" class="form-control form-control-lg" id="nacimiento">
+                  </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="genero">Género:</label>
+                      <select class="form-control form-control-lg" id="genero" name="sellist1">
+                        <option></option>
+                        <option>Femenino</option>
+                        <option>Masculino</option>
+                        <option>Otro</option>
+                      </select>
+                  </div> 
+                  </div>
+                </div>
+              </section>
+              <!-- Step 2 -->
+              <h5>Información de Contacto</h5>
+              <section>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="correo">Correo Electrónico: </label>
+                      <input type="email" class="form-control form-control-lg" id="correo">
+                  </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="telefono">Teléfono: </label>
+                      <input type="text" class="form-control form-control-lg" id="telefono">
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="form-group">                   
+                      <label for="direccion">Dirección:</label>
+                      <textarea class="form-control" rows="5" id="direccion"></textarea>
+                  </div>
+                  </div>
+                </div>
+              </section>
+              <!-- Step 3 -->
+              <h5>Datos de Empleo</h5>
+              <section>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="salario">Salario: </label>
+                      <input type="text" class="form-control form-control-lg" id="salario">
+                    </div>
+                    <div class="form-group">
+                      <label for="cargo">Cargo:</label>
+                      <select class="form-control form-control-lg" id="cargo" name="sellist1">
+                          <option></option>
+                          <option>Cajero</option>
+                          <option>Dependienta</option>
+                          <option>Bodeguero</option>
+                          <option>Gerente</option>
+                     </select>
+                    </div>
+                    <div class="form-group">
+                      <label for="tipo">Tipo de Empleado:</label>
+                      <select class="form-control form-control-lg" id="tipo" name="sellist1">
+                         <option></option>
+                         <option>Permanente</option>
+                         <option>Por contrato</option>
+                         <option>Por Horas</option>
+                         <option>4</option>
+                     </select>
+                   </div>
+                   <div class="form-group">
+                    <label for="ingreso">Fecha de Ingreso: </label>
+                    <input type="date" class="form-control form-control-lg" id="ingreso">
+                   </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="salida">Fecha de Salida: </label>
+                      <input type="date" class="form-control form-control-lg" id="salida">
+                   </div>
+                   <div class="form-group">
+                       <label for="estatus">Estatus:</label>
+                       <select class="form-control form-control-lg" id="estatus" name="sellist1">
+                          <option></option>
+                          <option>Activo</option>
+                          <option>Inactivo</option>                    
+                       </select>
+                   </div>
+                   <div class="form-group">
+                    <label for="foto">Foto: </label>
+                    <input type="file" class="form-control-file border form-control-lg" name="foto">
+                  </div>
+                  </div>
+                </div>
+              </section>
+              <!-- Step 4 -->
+              <h5>Usuario</h5>
+              <section>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="usuario">Nombre de Usuario: </label>
+                      <input type="text" class="form-control form-control-lg" id="usuario">
+                    </div>                
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="password">Password: </label>
+                      <input type="text" class="form-control form-control-lg" id="password">
+                    </div>   
+                  </div>
+                </div>
+               
+               
+              </section>
+            </form>
+          </div>
         </div>
       </div>
       <div class="footer-wrap pd-20 mb-20 card-box">
@@ -275,6 +400,7 @@
   <script src="vendors/scripts/script.min.js"></script>
   <script src="vendors/scripts/process.js"></script>
   <script src="vendors/scripts/layout-settings.js"></script>
+  <script src="src/plugins/jquery-steps/jquery.steps.js"></script>
+  <script src="vendors/scripts/steps-setting.js"></script
 </body>
-
 </html>
