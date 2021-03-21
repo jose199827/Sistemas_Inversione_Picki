e47f75ba1b382f6ede14b6f3ada374f4d52bbb1e
LexGarMont97 <marioalexgarciamont@yahoo.com>
2021-03-20 21:06:45 -0600

    avance_alex_20/03/2021

--- adjclientes.php
+++ adjclientes.php
@@ -2,6 +2,10 @@
 <html>
 
 <head>
+  <!-- Basic Page Info -->
+  <meta charset="utf-8">
+  <title>Inversiones Picky</title>
+
   <!-- Site favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="vendors/images/favicon-32x32.png">
@@ -11,11 +15,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
   <!-- Google Font -->
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+    rel="stylesheet">
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
   <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
-  <link rel="stylesheet" type="text/css" href="src/plugins/jquery-steps/jquery.steps.css">
+  <link rel="stylesheet" type="text/css" href="src/plugins/datatables/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="src/plugins/datatables/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
 
 
@@ -23,7 +29,7 @@
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+    function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
 
     gtag('config', 'UA-119386393-1');
@@ -265,94 +271,150 @@
             </div>
 
 					<!-- EMPIEZA TABLA -->
-					
-  <div class="mobile-menu-overlay"></div>
-
-  <div class="main-container">
-    <div class="pd-ltr-20 xs-pd-20-10">
-      
-
-        <div class="pd-20 card-box mb-30">
+					<div class="pd-20 card-box mb-30">
           <div class="clearfix">
-            <h4 class="text-blue h4">Agregar nuevo proveedor</h4>
+            <h4 class="text-blue h4">Step wizard</h4>
+            <p class="mb-30">jQuery Step wizard</p>
           </div>
           <div class="wizard-content">
             <form class="tab-wizard wizard-circle wizard">
-              <h5>Banco</h5>
+              <h5>Personal Info</h5>
               <section>
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label >Nombre del Banco:</label>
+                      <label >First Name :</label>
                       <input type="text" class="form-control">
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label >Abreviatura :</label>
-                      <input type="text" class="form-control">
-                    </div>
-                  </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                      <label >Numero de Cuenta :</label>
+                      <label >Last Name :</label>
                       <input type="text" class="form-control">
                     </div>
                   </div>
                 </div>
-                
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label>Email Address :</label>
+                      <input type="email" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label>Phone Number :</label>
+                      <input type="text" class="form-control">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label>Select City :</label>
+                      <select class="custom-select form-control">
+                        <option value="">Select City</option>
+                        <option value="Amsterdam">India</option>
+                        <option value="Berlin">UK</option>
+                        <option value="Frankfurt">US</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label >Date of Birth :</label>
+                      <input type="text" class="form-control date-picker" placeholder="Select Date">
+                    </div>
+                  </div>
+                </div>
               </section>
               <!-- Step 2 -->
-              <h5>Personas</h5>
+              <h5>Job Status</h5>
               <section>
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label>Nombre del Contacto :</label>
+                      <label>Job Title :</label>
                       <input type="text" class="form-control">
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label>Nombre de la empresa :</label>
+                      <label>Company Name :</label>
                       <input type="text" class="form-control">
                     </div>
                   </div>
-                  <div class="col-md-6">
+                  <div class="col-md-12">
                     <div class="form-group">
-                      <label>RTN de la empresa :</label>
-                      <input type="text" class="form-control">
+                      <label>Job Description :</label>
+                      <textarea class="form-control"></textarea>
                     </div>
                   </div>
                 </div>
               </section>
               <!-- Step 3 -->
-              <h5>Informacion</h5>
+              <h5>Interview</h5>
               <section>
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label>Telefono :</label>
+                      <label>Interview For :</label>
                       <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Interview Type :</label>
+                      <select class="form-control">
+                        <option>Normal</option>
+                        <option>Difficult</option>
+                        <option>Hard</option>
+                      </select>
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label>Direccion:</label>
+                      <label>Interview Date :</label>
+                      <input type="text" class="form-control date-picker" placeholder="Select Date">
+                    </div>
+                    <div class="form-group">
+                      <label>Interview Time :</label>
+                      <input class="form-control time-picker" placeholder="Select time" type="text">
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <!-- Step 4 -->
+              <h5>Remark</h5>
+              <section>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label>Behaviour :</label>
                       <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Confidance</label>
+                      <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Result</label>
+                      <select class="form-control">
+                        <option>Select Result</option>
+                        <option>Selected</option>
+                        <option>Rejected</option>
+                      </select>
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label>Correo Electronico :</label>
-                      <input type="text" class="form-control">
+                      <label>Comments</label>
+                      <textarea class="form-control"></textarea>
                     </div>
                   </div>
                 </div>
               </section>
             </form>
           </div>
-        </div>
+      
 
 			</div>
 			<div class="footer-wrap pd-20 mb-20 card-box">
